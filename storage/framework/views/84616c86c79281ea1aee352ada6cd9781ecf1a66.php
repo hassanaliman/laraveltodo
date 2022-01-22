f9f0ae7a2b2ac14ff3046800018b60fbd1e585b0
hassanaliman <59656914+hassanaliman@users.noreply.github.com>
2022-01-22 14:15:00 +0700

    Add files via upload

---
+++ storage/framework/views/84616c86c79281ea1aee352ada6cd9781ecf1a66.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+    <div id="app">
+  <?php echo e(message); ?>
+
+</div>
+    </body>
+    <script src="<?php echo e(mix('js/app.js')); ?>"></script>
+</html>
+<?php /**PATH D:\todolaravel\resources\views/welcome.blade.php ENDPATH**/ ?>
